e9f23c85b46178eacfb32a58b961e5509f7d7e50
DESKTOP-EH88G91\Tautv <tautvydas.armonas@ktu.edu>
2018-05-08 15:14:14 +0300

    test commit

--- src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
+++ src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
@@ -10,5 +10,5 @@ public class GamifiedcourseApplication {
         SpringApplication.run(GamifiedcourseApplication.class, args);
     }
     //Saimonas test
-    //Armonas test
+    //Armonas test again
 }
