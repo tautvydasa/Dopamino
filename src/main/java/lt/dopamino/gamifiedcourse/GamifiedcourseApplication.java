fb399c6072d848c467a8ce53912e870bf5225dde
simpledare <nesuspameris@gmail.com>
2018-05-08 14:44:45 +0300

    initial commit

---
+++ src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
@@ -0,0 +1,12 @@
+package lt.dopamino.gamifiedcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifiedcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamifiedcourseApplication.class, args);
+    }
+}
