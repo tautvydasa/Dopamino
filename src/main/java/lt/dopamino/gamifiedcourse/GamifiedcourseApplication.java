1f68aa72ca30ed43c201d71025c1aea0acdb05fa
Saimonas Vaičkus <outoflock377@gmail.com>
2018-05-08 15:08:29 +0300

    test commit

--- src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
+++ src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
@@ -9,4 +9,5 @@ public class GamifiedcourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(GamifiedcourseApplication.class, args);
     }
+    //Saimonas test
 }
