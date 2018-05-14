8c2c24a84c6ff7a4e34d6ee83475104b258a1b58
SC\donsta1 <don.stankevicius@ktu.edu>
2018-05-14 11:18:38 +0300

    main contoller

---
+++ src/main/java/lt/dopamino/gamifiedcourse/controller/MainController.java
@@ -0,0 +1,14 @@
+package lt.dopamino.gamifiedcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "mainPage";
+    }
+
+}
