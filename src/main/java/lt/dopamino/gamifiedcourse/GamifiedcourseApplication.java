affc02101b52c66dd07b87478240437fcd849c13
simpledare <nesuspameris@gmail.com>
2018-05-14 12:29:49 +0300

    Mindės patvarkymai visad padeda. KUDOS Mindei

--- src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
+++ src/main/java/lt/dopamino/gamifiedcourse/GamifiedcourseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GamifiedcourseApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GamifiedcourseApplication.class, args);
     }
     //Saimonas test
