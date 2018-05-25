7e219b2fc622207e467813e33e25468f7ad945c7
Tautvydas Armonas <Tautvydas.aisq@gmail.com>
2018-05-25 17:58:00 +0300

    Forum update: courses, posts and comments lists added; 'add comment' and 'delete post' functions don't work yet. Added good examples how to link pages.

--- src/main/java/lt/dopamino/gamifiedcourse/StudentTeacher/Controllers/StudentTeacherController.java
+++ src/main/java/lt/dopamino/gamifiedcourse/StudentTeacher/Controllers/StudentTeacherController.java
@@ -4,36 +4,45 @@
 
 package lt.dopamino.gamifiedcourse.StudentTeacher.Controllers;
 
+import lt.dopamino.gamifiedcourse.Model.Comment;
+import lt.dopamino.gamifiedcourse.Model.Post;
+import lt.dopamino.gamifiedcourse.Model.Repository.CommentRepository;
 import lt.dopamino.gamifiedcourse.Model.Repository.CourseRepository;
+import lt.dopamino.gamifiedcourse.Model.Repository.PostRepository;
 import lt.dopamino.gamifiedcourse.Model.Repository.StudentRepository;
 import lt.dopamino.gamifiedcourse.Model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student_teacher")
 public class StudentTeacherController {
 
     private final StudentRepository studentRepository;
+
+    private final PostRepository postRepository;
+
     private final CourseRepository courseRepository;
 
-    @Autowired
-    public StudentTeacherController(StudentRepository studentRepository, CourseRepository courseRepository) {
+    private final CommentRepository commentRepository;
 
-        this.courseRepository = courseRepository;
+    @Autowired
+    public StudentTeacherController(StudentRepository studentRepository, PostRepository postRepository, CourseRepository courseRepository, CommentRepository commentRepository) {
         this.studentRepository = studentRepository;
+        this.postRepository = postRepository;
+        this.courseRepository = courseRepository;
+        this.commentRepository = commentRepository;
     }
 
     @GetMapping
     public String openMainPage() {
         return "Teacher/Views/StudentTeacherMainPage";
     }
-
 
     public void openCourseTest() {
 
@@ -65,35 +74,53 @@ public class StudentTeacherController {
 
     @GetMapping("/courses")
     public String openCourses(Model model) {
-        model.addAttribute("allCourses", courseRepository.findAll());
         return "Teacher/Views/CoursesPage";
     }
 
-    @GetMapping("/forum_courses")
-    public String openForumCourses(Model model) {
-        return "Teacher/Views/ForumCoursesPage";
+    @GetMapping("/courses_forum")
+    public String openCoursesForum(Model model) {
+        model.addAttribute("allCourses", courseRepository.findAll());
+        return "Teacher/Views/CoursesForumPage";
     }
 
+    @GetMapping(value = "/courses_forum/{id}")
+    public String showPosts(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("course", courseRepository.findById(id).get());
+        model.addAttribute("allPosts", postRepository.getPostsById(id));
+        return "Teacher/Views/CourseForumPage";
+    }
+
+    @GetMapping(value = "/courses_forum/{id}/{id2}")
+    public String showPost(Model model, @PathVariable("id") Integer id, @PathVariable("id2") Integer id2) {
+        model.addAttribute("course", courseRepository.findById(id).get());
+        model.addAttribute("post", postRepository.findById(id2).get());
+        model.addAttribute("allComments", commentRepository.getCommentsById(id2));
+        return "Teacher/Views/PostPage";
+    }
+
+    @GetMapping("/courses_forum/{id}/{id2}/write_comment")
+    public String writeComment(Model model, @PathVariable("id") Integer id, @PathVariable("id2") Integer id2) {
+        model.addAttribute("course", courseRepository.findById(id).get());
+        model.addAttribute("post", postRepository.findById(id2).get());
+        Student student = (Student) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("comment", new Comment());
+        return "Teacher/Views/WriteCommentPage";
+    }
+
+    @GetMapping(value = "/courses_forum/{id}/delete/{id2}")
+    public String deletePost(Model model, @PathVariable("id2") Integer id2) {
+        System.out.println(id2);
+        System.out.println("test");
+        return "redirect:/courses_forum/{id}";
+    }
 
     public void submitRate() {
 
     }
 
-    @GetMapping(value = "/courses/{id}")
-    public String showPosts(Model model, @PathVariable("id") Integer id) {
-        Student student = (Student) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("student", student);
-        model.addAttribute("course", courseRepository.getCourseById(id));
-        return "Teacher/Views/CourseInfoPage";
+    public void openCourse() {
+
     }
-
-
-
-/*    @GetMapping("/course/{id}")
-    public String openCourse(Model model, @PathVariable("id") int id) {
-        Course course = courseRepository.findById(id).get();
-        return "????";
-    }*/
 
     public void openCourseLeaderboard() {
 
