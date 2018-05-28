bba37c0c93d31ed0fc03b38c9eda2fc6e5a3d269
Saimonas Vaičkus <outoflock377@gmail.com>
2018-05-28 14:41:07 +0300

    Points reward for Student

--- src/main/java/lt/dopamino/gamifiedcourse/Model/Repository/ResultRepository.java
+++ src/main/java/lt/dopamino/gamifiedcourse/Model/Repository/ResultRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface ResultRepository extends JpaRepository<Result, Integer> {
     @Query("select coalesce(max(m.mark), 0) from Result m where m.courseSection.id = ?1 and m.student.id = ?2")
     double getBestSectionMark(int courseSectionId, int studentId);
+
+    @Query("select count(distinct m.courseSection) from Result m where m.course.id = ?1 and m.student.id = ?2")
+    Integer getDoneTestCount(int courseId, int studentId);
 }
